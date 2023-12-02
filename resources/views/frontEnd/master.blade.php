8a36d0c12e79f959c03c4e61d68882fcb4d9e280
YanaWebProgrammer <teukita15@gmail.com>
2023-12-02 17:12:10 +0700

    first update

---
+++ resources/views/frontEnd/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('blog')}}/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 class="judul">MyWebsite</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="hero"></div>
+
+        <div class="content cf">
+            <div class="main">
+                <h2>Judul Article</h2>
+                <p class="penulis">ditulis oleh <a href="https://github.com/Yanasurya1989" target="_blank">Yana</a>. pada 24 Agustus 2023</p>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, animi porro dolores dicta accusantium error sint hic consequuntur nulla nam cumque excepturi, esse autem aspernatur aliquid iste corrupti aut numquam.</p>
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde accusantium, repellat ipsum odio corporis minima pariatur alias delectus ut distinctio doloremque eum. Est nobis possimus quod minima officiis sit ea, velit consequuntur cupiditate id aliquam ad reprehenderit inventore laudantium recusandae distinctio necessitatibus aliquid, libero quas, excepturi ipsum! In ut reiciendis quibusdam est veritatis nam ea architecto atque aspernatur dolor doloribus tempore assumenda saepe magni illum quam accusamus voluptas delectus, cumque sequi corrupti! Aliquam similique, quidem ducimus incidunt ex iste. Non similique ipsa natus, ipsam, cumque sunt nobis vero iure doloremque nisi neque eaque pariatur nam nesciunt quidem a atque nihil?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae suscipit exercitationem, numquam vel, minima itaque quibusdam soluta voluptates debitis, quod expedita atque! Ipsa doloribus perferendis asperiores sed, veniam at quibusdam saepe blanditiis? Quo veritatis alias, delectus tenetur sit distinctio, magnam ad provident cumque, necessitatibus sint fuga dolores atque adipisci blanditiis quod animi perferendis doloremque eum maiores nobis odio aperiam? Cupiditate, cumque at. Ipsam laborum animi assumenda adipisci architecto nulla mollitia eius vitae sapiente fugit consequatur provident perspiciatis, quam quas sequi.</p>
+            </div>
+            <div class="sidebar">
+                <h3>Tentang Penulis</h3>
+                <img src="image/Yana.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam debitis fugiat est praesentium ea quo, dignissimos quos nemo laboriosam labore soluta ullam ipsum maxime dolores rem sapiente voluptatibus perferendis ab?</p>
+            </div>
+        </div>
+
+        <div class="footer">
+            <p class="copy">Copyright 2023, Yana. </p>
+        </div>
+    </div>
+</body>
+</html>
